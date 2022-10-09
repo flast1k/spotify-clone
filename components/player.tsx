1e11f827907ede4d21294230181769d62ca7de66
Artem Smyslov <Artem.Smyslov@alemira.com>
2022-10-09 12:14:57 +0300

    feat: player init

---
+++ components/player.tsx
@@ -0,0 +1,26 @@
+import {
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from '@chakra-ui/react'
+import ReactHowler from 'react-howler'
+import { useEffect, useRef, useState } from 'react'
+import {
+  MdShuffle,
+  MdSkipPrevious,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from 'react-icons/md'
+import { useStoreActions } from 'easy-peasy'
+
+export const Player = () => {
+  return <Box>hello from player</Box>
+}
