3eebb6e6d1bda719f66b7ec2b50500b0c231cb5c
Artem Smyslov <Artem.Smyslov@alemira.com>
2022-10-09 11:46:08 +0300

    fix: fix auth server side

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -36,11 +36,18 @@ const Playlist = ({ playlist }) => {
 }
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+  let user
+
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+  } catch (error) {
+    return { redirect: { permanent: false, destination: '/signin' } }
+  }
+
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: Number(query.id),
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
